cf88de8f85cf9dcbc640d31f9f9ab2f761dd868f
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-14 01:35:09 +0200

    When

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,29 @@
 fun main() {
     print("Enter any number: ")
     val x = readln().toInt()
-    if (x > 0)
-        println("x is positive.")
-    else if(x < 0)
-        println("x is negative.")
-    else
-        println("x is 0.")
 
-
-    print("Enter your degree: ")
-    val degree = readln().toInt()
-
-    if(degree >= 50)
-        println("ناجح")
-    else {
-        if(degree >= 45)
-            println("ناجح بقرار")
-        else
-            println("راسب")
+    when(x) {
+        0-> {
+            println("x is 0.")
+        }
+        1-> {
+            println("x is 1.")
+        }
+        else-> {
+            println("unknown!")
+        }
     }
 
+    print("Enter your name: ")
+    val name = readln()
+
+    when(name) {
+        "Mohamed"-> {
+            println("Accepted.")
+        }
+
+        else-> {
+            println("Only admin can login.")
+        }
+    }
 }
