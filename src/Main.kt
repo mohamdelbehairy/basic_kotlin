91aa955b2badada3b4243f982471f3db84bda5c7
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-13 22:55:23 +0200

    println,val,var,int,char,double

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
+    // val => value can't update
+    // var => value can update
     println("Hello World!")
+
+    val x = 5
+    val y = 10
+    val z = 10.5
+    val d = '^'
+
+    println(x)
+    println(y)
+    println(z)
+    println(d)
 }
