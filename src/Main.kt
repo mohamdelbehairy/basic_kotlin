9dfb5a9c0fe4efda4eb99e71a7f8cc49e0a91064
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-16 02:51:18 +0200

    Logic Operation

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,15 @@
 fun main() {
-    print("Enter number1: ")
+    println("Enter any two numbers:")
     val num1 = readln().toInt()
-
-    print("Enter number2: ")
     val num2 = readln().toInt()
 
-    println("sum = ${num1+num2}")
-    println("sub = ${num1-num2}")
-    println("mul = ${num1*num2}")
-    println("div = ${num1/num2}")
-    println("mod = ${num1%num2}")
+    println("num1 > num2 : ${num1>num2}")
+    println("num1 < num2 : ${num1<num2}")
+    println("num1 == num2 : ${num1==num2}")
+    println("num1 >= num2 : ${num1>=num2}")
+    println("num1 <= num2 : ${num1<=num2}")
+
+    println("num1 > 9 && num2 > 2 : ${num1>9 && num2>2}")
+    println("num1 > 9 || num2 > 2 : ${num1>9 || num2>2}")
+    println("num1 != num2 : ${num1!=num2}")
 }
