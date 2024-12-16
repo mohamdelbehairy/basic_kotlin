3dfe46f571bcbd2209b0d3a77d164af8bb87d923
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-16 09:39:13 +0200

    Control Statements

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,27 @@
 fun main() {
 
+//    for (x in 1..10) {
+//        println("x: $x")
+//        continue
+//    }
+//    for (x in 1..10) {
+//        for (y in 1..10) {
+//            println("$x * $y = ${x*y}")
+//            break
+//        }
+//    }
 
-    for (x in 1..10) {
-        for (y in 1..10) {
-            println("$x * $y = ${x * y}")
-        }
+    // بقدر احط اسماء للفورلوب واعمله كونتنيو او بريك عن طريق الاسم
+    loop1@for (x in 1..10) {
+
+        if (x == 5)break@loop1
+        println("x: $x")
     }
-    //بيستخدم لعرض عناصر مصفوفه معينه
-    val x = "welcome"
-    for (i in x) {
-        println("Char: $i")
+    loop2@for (x in 1..10) {
+        loop3@for (y in 1..10) {
+
+            if(y == 5) break@loop3 // == if(y == 5) continue@loop2
+            println("$x * $y = ${x*y}")
+        }
     }
 }
