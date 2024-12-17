458223fa46fdd6b4d62e8b363b25b4a27e0be484
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-17 21:34:57 +0200

    For Each

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,17 @@
 fun main() {
-    val x = ArrayList<Int>()
-    x.add(3);x.add(5);x.add(4);x.add(3);x.add(6)
+    val x = arrayOf(2,4,6,8,10)
+    val y = ArrayList<Int>()
+    y.add(1);y.add(3);y.add(5);y.add(7);y.add(9)
 
-    println("x: $x")
-    println("size: ${x.size}")
-    println("last index of (3): ${x.lastIndexOf(3)}") // بيعرف اخر انديكيس للعنصر بتاعي عشان لو موجود اكتر من مره
-    println("first: ${x.first()}")
-    println("last: ${x.last()}")
-    println("index of: ${x.indexOf(6)}")
-    println("x is empty: ${x.isEmpty()}")
-    println("x is not empty: ${x.isNotEmpty()}")
-    println("x is contains(4): ${x.contains(4)}")
-    println("x is contains(7): ${x.contains(7)}")
-    x[2] = 7 // x.set(2,7)
-    println("x: $x")
-    x.toArray()
-    println("x: $x")
-    x.clear()
-    println("size: ${x.size}")
+    // for statement
+    for (i in x) {
+        println("for x: $i")
+    }
+    for (i in y) {
+        println("for y: $i")
+    }
+
+    // forEach expression
+    x.forEach {  println("forEach x : $it") }
+    y.forEach {  println("forEach x : $it") }
 }
