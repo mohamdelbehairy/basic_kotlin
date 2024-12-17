78c055b62238829461a66c4234fb4103e4e638df
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-17 21:19:45 +0200

    Array List (Dealing With Function)

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,21 @@
 fun main() {
-    val x = Array(3) {0} // immutable غير قابله للتعديل
-    val y = ArrayList<Int>() // mutable قابله للتعديل
+    val x = ArrayList<Int>()
+    x.add(3);x.add(5);x.add(4);x.add(3);x.add(6)
 
-    x[0] = 1
-    y.add(3)
-    y.add(1,5)
-    println("y: $y")
-    y.removeAt(1) // remove index
-    y.remove(3) // remove element
-    println("y: $y")
-    y.add(5)
-    y.add(6)
-    println("y: $y")
-    y.clear() // clear array list
-    println("y: $y")
+    println("x: $x")
+    println("size: ${x.size}")
+    println("last index of (3): ${x.lastIndexOf(3)}") // بيعرف اخر انديكيس للعنصر بتاعي عشان لو موجود اكتر من مره
+    println("first: ${x.first()}")
+    println("last: ${x.last()}")
+    println("index of: ${x.indexOf(6)}")
+    println("x is empty: ${x.isEmpty()}")
+    println("x is not empty: ${x.isNotEmpty()}")
+    println("x is contains(4): ${x.contains(4)}")
+    println("x is contains(7): ${x.contains(7)}")
+    x[2] = 7 // x.set(2,7)
+    println("x: $x")
+    x.toArray()
+    println("x: $x")
+    x.clear()
+    println("size: ${x.size}")
 }
