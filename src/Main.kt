9db8fcf357ce9433bd7df3c31a1c2e725505453a
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-17 20:13:57 +0200

    Use For in Array

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,16 @@
 fun main() {
-    val x = IntArray(3) // first way to make array
-    x[0] = 1
-    x[1] = 2
-    x[2] = 3
-    println("x = ${x.toList()}")
+    val x = arrayOf("Mohamed",2,'^',5.5,true)
 
-    val y = Array(3) {0} // second way to make array
-    // {} => initial value of array
-    // 0 => make type of array int, 0.0 => make type of array double, "" => make type of array string
-    y[0] = 4
-    y[1] = 5
-    y[2] = 6
-    println("y = ${y.toList()}")
+//    println("#################")
+//    for (i in 0 until  x.size) {
+//        println("x[$i] = ${x[i]}")
+//    }
 
-    val z = Array<Int>(3){0} // third way to make array
-    z[0] = 7
-    z[1] = 8
-    z[2] = 9
-    println("z = ${z.toList()}")
-
-    val a = arrayOf(10,11,12) // best way
-    println("a = ${a.toList()}")
-
-    val b = arrayOf("Mohamed","Ahmed",12,10.5,true)
-    println("a = ${b.toList()}")
+    println("Enter the value of array: ")
+    for (i in 0 until x.size) {
+        x[i] = readln()
+    }
+    for (i in x) {
+        println(i)
+    }
 }
