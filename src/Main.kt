d2253a5fbc47fcd33c8be39b8a64ad8cbd9f646a
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-17 19:58:07 +0200

    Array

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,27 @@
 fun main() {
+    val x = IntArray(3) // first way to make array
+    x[0] = 1
+    x[1] = 2
+    x[2] = 3
+    println("x = ${x.toList()}")
 
-   loop1@while (true) {
-       try {
-           print("Enter you age: ")
-           val age = readln().toInt()
-           println("your age is $age years")
-           break@loop1
-       } catch (e:Exception) {
-           println("You entered wrong value.")
-           continue@loop1
-       }
-   }
+    val y = Array(3) {0} // second way to make array
+    // {} => initial value of array
+    // 0 => make type of array int, 0.0 => make type of array double, "" => make type of array string
+    y[0] = 4
+    y[1] = 5
+    y[2] = 6
+    println("y = ${y.toList()}")
 
-    println("Done!")
+    val z = Array<Int>(3){0} // third way to make array
+    z[0] = 7
+    z[1] = 8
+    z[2] = 9
+    println("z = ${z.toList()}")
+
+    val a = arrayOf(10,11,12) // best way
+    println("a = ${a.toList()}")
+
+    val b = arrayOf("Mohamed","Ahmed",12,10.5,true)
+    println("a = ${b.toList()}")
 }
