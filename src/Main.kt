c75b2bca7e897fc4c1a4a7d47d9e051b2c01b422
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-17 20:24:08 +0200

    Array (Dealing With Function)

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,22 @@
 fun main() {
-    val x = arrayOf("Mohamed",2,'^',5.5,true)
+    val arr = arrayOf(58,5,15,3,25,99,1,6)
+    println("arr: ${arr.toList()}")
 
-//    println("#################")
-//    for (i in 0 until  x.size) {
-//        println("x[$i] = ${x[i]}")
-//    }
+    println("max element: ${arr.max()}")
+    println("min element: ${arr.min()}")
+    println("count element: ${arr.count()}")
+    println("size element: ${arr.size}")
+    println("first element: ${arr.first()}")
+    println("last element: ${arr.last()}")
 
-    println("Enter the value of array: ")
-    for (i in 0 until x.size) {
-        x[i] = readln()
-    }
-    for (i in x) {
-        println(i)
+    println("set element: update index 3 by the value 333")
+    arr.set(3,333)  // arr[3] = 333
+    println("get element: ${arr.get(3)}") // arr[3]
+
+    arr.sort()
+    println("sort element: ${arr.toList()}")
+
+    for (i in 0 until arr.size) {
+        println("arr[$i] = ${arr[i]}")
     }
 }
