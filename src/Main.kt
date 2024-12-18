90a0f54b9fe7467258e649895804aef5fe172761
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-18 04:58:37 +0200

    Function Part 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,9 @@
 fun main() {
-    val map = mapOf(Pair(1,"A"), Pair(2,"B"),Pair(3,"C"))
-    val mapM = mutableMapOf<Int,String>()
-    mapM[0] = "a" // mapM.set(0,"a") || mapM.put(4,"e")
-    mapM[1] = "b";mapM[2] = "c"
-    mapM[44] = "z"
+    func1()
+}
 
-    println("map: $map")
-    println("mapM: $mapM")
-    mapM.replace(44,"e")
-    println("mapM: $mapM")
-
-    val c = HashMap<Int,String>() // mutableMapOf
-    c[0] = "a"; c[1] = "b";c[2] = "c";c[44] = "z"
-
-    println("c: $c")
+fun func1() {
+    print("Enter your name: ")
+    val name = readln()
+    println("your name is $name.")
 }
