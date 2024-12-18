7c399b0b588a75a428af77281f22dc93c47e47c3
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-18 06:33:47 +0200

    Passing Array Through Function

--- src/Main.kt
+++ src/Main.kt
@@ -1,28 +1,22 @@
 fun main() {
-    println("func1 = ${func1()}")
-    println("func2 = ${func2()}")
-    println("func3 = ${func3()}")
-    println("func4 = ${func4(5,5)}")
+//    val x:Array<Any> = arrayOf(2,"mohamed",'@',10.5,true)
+    val y = ArrayList<Any>()
+    input(y,5);output(y)
 }
 
-fun func1(): Int {
-    val x = 10
-    val y = 10
-    return (x+y)
+
+fun input(x:ArrayList<Any>,size:Int) {
+    println("Enter the elements of array: ")
+//    for (i in x.indices) {
+//        x[i] = readln()
+//    }
+    for (i in 0..< size) {
+        x.add(readln())
+    }
 }
 
-fun func2(): String {
-    val x = "Mohamed"
-    val y = " Elbehairy"
-    return (x+y)
-}
-
-fun func3(): Boolean {
-    val x = 10
-    val y = 5
-    return (x > y)
-}
-
-fun func4(x:Int,y:Int):Int {
-    return (x+y)
+fun output(x:ArrayList<Any>) {
+    for (i in x.indices) {
+        println("x[$i] = ${x[i]}")
+    }
 }
