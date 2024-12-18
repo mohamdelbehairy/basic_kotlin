8aac5f9c3deeec0521a4721fb7deae82d345ae27
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-18 06:06:51 +0200

    Function Part 2(return)

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,28 @@
 fun main() {
-    func1()
+    println("func1 = ${func1()}")
+    println("func2 = ${func2()}")
+    println("func3 = ${func3()}")
+    println("func4 = ${func4(5,5)}")
 }
 
-fun func1() {
-    print("Enter your name: ")
-    val name = readln()
-    println("your name is $name.")
+fun func1(): Int {
+    val x = 10
+    val y = 10
+    return (x+y)
+}
+
+fun func2(): String {
+    val x = "Mohamed"
+    val y = " Elbehairy"
+    return (x+y)
+}
+
+fun func3(): Boolean {
+    val x = 10
+    val y = 5
+    return (x > y)
+}
+
+fun func4(x:Int,y:Int):Int {
+    return (x+y)
 }
