19b302c83c0456f9bfe36f3dfd11662dc0361d34
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-19 23:23:29 +0200

    Builtin Function

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,32 @@
-fun main() {
-    print("Enter a positive integer: ")
-    val x = readln().toInt()
-    println("sum = ${add(x)}")
-}
+import kotlin.math.*
 
-fun add(c:Int):Int {
-    return if(c != 0) c + add(c-1) // 5 + 4 + 3 + 2 + 1 => 15
-    else 0
+fun main() {
+    val x = -5.0
+
+    println("x = $x")
+    println("sign(x) = ${sign(x)}")
+    println("x.sign = ${x.sign}")
+    println("|$x| = ${abs(x)}")
+
+    println("floor(3.5) = ${floor(3.5)}")
+    println("ceil(3.5) = ${ceil(3.5)}")
+    println("round(3.5) = ${round(3.5)}")
+
+    println("sin30 = ${sin(30.0* PI/180)}")
+    println("cos30 = ${cos(60.0* PI/180)}")
+    println("tan30 = ${tan(45.0* PI/180)}")
+    println("sin^-1(0.5)) = ${asin(0.5)*180/ PI}")
+    println("cos^-1(0.5)) = ${acos(0.5) *180/ PI}")
+    println("tan^-1(1)) = ${atan(1.0) *180/ PI}")
+
+    println("log100 = ${log(100.0,10.0)}")
+    println("log10 = ${log10(10.0)}")
+    println("ln10 = ${ln(10.0)}")
+    println("e^3 = ${exp(3.0)}")
+
+    println("min(2,7) = ${min(2,7)}")
+    println("max(2,7) = ${max(2,7)}")
+    println("sqrt(25.0) = ${sqrt(25.0)}")
+    println("pow(2) = ${x.pow(2)}")
 }
 
