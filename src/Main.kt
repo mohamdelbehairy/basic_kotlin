812f708383b8dcf5357df486dd1026c375f6d9ca
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-21 07:20:51 +0200

    Higher Order Function 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,12 @@
 fun main() {
-   println("sum: ${ sum(1,6,lambda4)}")
+    var total = 0
+    sum(1,6) {a,b -> total = a+b} // ==  sum(1,6,{a,b -> a+b}) == ::myFunc
+  println("sum: $total")
+
 }
 
-fun sum(a:Int,b:Int,myFunc:(Int,Int)->Int):Int {
-    return myFunc(a,b)
+fun sum(a:Int,b:Int,myFunc:(Int,Int)->Unit) {
+    myFunc(a,b)
 }
 
 
@@ -11,3 +14,7 @@ val lambda1 = { println("Hello from lambda1.") }
 val lambda2 = {name:String -> println("My name is $name.") }
 val lambda3 = {5+10}
 val lambda4 = {a:Int,b:Int -> a+b}
+
+fun myFunc(a:Int,b:Int):Int {
+    return a+b
+}
