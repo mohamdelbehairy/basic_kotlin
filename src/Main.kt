0a2020ca7337f9a9ab2e503e06445452f0aabb47
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-21 08:47:10 +0200

    Higher Order Function 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,21 @@
 fun main() {
-    var total = 0
-    sum(1,6) {a,b -> total = a+b} // ==  sum(1,6,{a,b -> a+b}) == ::myFunc
-  println("sum: $total")
+    val name = "Mohamed"
 
+    println(name.reversed())
+    println(funcRevers(name))
+    println(lambdaReverse(name))
+    higherFunc(name,::funcRevers)
+    higherFunc(name,lambdaReverse)
+    higherFunc(name) {word:String-> word.reversed()}
+    higherFunc(name) {it.reversed()}
 }
 
-fun sum(a:Int,b:Int,myFunc:(Int,Int)->Unit) {
-    myFunc(a,b)
+fun higherFunc(word:String,myFunc:(String)->String) {
+   println( myFunc(word))
 }
 
-
-val lambda1 = { println("Hello from lambda1.") }
-val lambda2 = {name:String -> println("My name is $name.") }
-val lambda3 = {5+10}
-val lambda4 = {a:Int,b:Int -> a+b}
-
-fun myFunc(a:Int,b:Int):Int {
-    return a+b
+fun funcRevers(word:String):String {
+    return word.reversed()
 }
+
+val lambdaReverse = {word:String-> word.reversed()}
