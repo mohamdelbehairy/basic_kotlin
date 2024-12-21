54550575d1bec956186c76cf0f5c7391c0352fe6
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-21 05:26:10 +0200

    Higher Order Function 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,41 +1,13 @@
 fun main() {
-    func1()
-    lambda1()
-
-    func2("Mohamed Elbehairy")
-    lambda2("Mohamed Elbehairy")
-
-    println(func3())
-    println(lambda3())
-
-    println(func4(10,5))
-    println(lambda4(10,5))
+   println("sum: ${ sum(1,6,lambda4)}")
 }
- // one
-fun func1() {
-    println("Hello from func1.")
+
+fun sum(a:Int,b:Int,myFunc:(Int,Int)->Int):Int {
+    return myFunc(a,b)
 }
+
 
 val lambda1 = { println("Hello from lambda1.") }
-
-// two
-
-fun func2(name:String) {
-    println("My name is $name.")
-}
-
 val lambda2 = {name:String -> println("My name is $name.") }
-
-// three
-fun func3():Int {
-    return (5+10)
-}
-
-val lambda3:()-> Int = {5+10}
-
-// four
-fun func4(a:Int,b:Int):Int {
-    return (a*b)
-}
-
-val lambda4:(Int,Int)-> Int = {a:Int,b:Int -> a*b}
+val lambda3 = {5+10}
+val lambda4 = {a:Int,b:Int -> a+b}
