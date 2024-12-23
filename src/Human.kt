8cb2de42739fd2e975d899b2873e490cdbe6db2d
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-23 11:21:34 +0200

    Constructor

--- src/Human.kt
+++ src/Human.kt
@@ -1,19 +1,16 @@
-class Human {
-    var name:String? = null
-    var gender:String? = null
-    var length:Int? = null
+class Human(age:Int,name:String) {
+    private var age:Int? = null
+    private var name:String? = null
 
-    fun introduce(a:Int) {
-        this.length  = this.length?.plus(a)
-        println("My name is "+this.name+".")
-        println("I am "+this.gender+".")
-        println("My length is ${this.length} cm.")
+//    constructor(p:Int){
+//        this.age = p
+//        println("age: $age")
+//    }
+
+    init {
+        this.age = age
+        this.name = name
+        println("age: $age")
+        println("name: $name")
     }
-
-//    fun  name() {
-//        println("My name is "+this.name+".")
-//    }
-//    fun  gender() {
-//        println("I am "+this.gender+".")
-//    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,3 @@
 fun main() {
-    println("Enter the Attributes of person 1: ")
-    val h1 = Human()
-    h1.name = readln()
-    h1.gender = readln()
-    h1.length = readln().toInt()
-
-    println("Enter the Attributes of person 2: ")
-    val h2 = Human()
-    h2.name = readln()
-    h2.gender = readln()
-    h2.length = readln().toInt()
-
-    h1.introduce(10)
-    h2.introduce(15)
+    Human(age = 23, name = "Mohamed Elbehairy")
 }
