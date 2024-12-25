80a0a7db2cc5386b5b32dd9af130eeb84d8d6613
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-12-25 13:07:05 +0200

    Super and Constructor

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,40 @@
 fun main() {
-    val b = B()
-    b.myAge2()
+    val b = C(15)
+//    b.myAge2()
 }
 
 
-open class A { // super class -> B, indirect super class c
+open class A {
      var age:Int? = 10
+
+    init {
+        println("Hello from constructor A.")
+    }
 
     fun myAge() {
         println("my age: ${this.age}")
     }
 }
 
-open class B: A()  { // subclass -> A, direct super class -> C
+open class B: A {
      private var age2:Int? = 20
 
     fun myAge2() {
         println("age: ${super.age}")
         println("age2: ${this.age2}")
     }
+    constructor() {
+        println("Hello from constructor B.")
+
+    }
+    constructor(a:Int) {
+        println("Hello from constructor B with $a.")
+
+    }
 }
 
-class C: B()  // subclass -> B,A
+class C(n: Int) : B(n) {
+    init {
+        println("Hello from constructor C.")
+    }
+}
